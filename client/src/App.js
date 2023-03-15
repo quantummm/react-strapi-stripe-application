f93368a93307d6e17518393ef1cde57b1c27139c
quantummm <aidenwang0826@gmail.com>
2023-03-15 22:54:25 +1100

    feat: add layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,43 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+
+const Layout = () => {
+  return(
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <span>Home</span>
+    element: <Layout />,
+    children:[
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/products/:id",
+        element: <Products />
+      },
+      {
+        path: "/product/:id",
+        element: <Product />
+      }
+    ]
   },
-  {
-    path: "/products/:id",
-    element: <span>Category</span>
-  },
-  {
-    path: "/product/:id",
-    element: <span>Product</span>
-  }
 ])
 
 
