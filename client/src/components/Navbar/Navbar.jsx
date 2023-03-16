6e065bc50e263b800cb15ad02e0f7694d7f1fdd4
quantummm <aidenwang0826@gmail.com>
2023-03-16 23:45:47 +1100

    feat: add navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Link } from 'react-router-dom';
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className='left'>
+          <div className="item">
+            <img src="/img/en.png" alt="" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/3">Children</Link>
+          </div>
+        </div>
+        <div className='center'>
+          <Link to="/">LAMASTORE</Link>
+        </div>
+        <div className='right'>
+          <div className="right">
+            <Link to="/">Homepage</Link>
+          </div>          
+          <div className="right">
+            <Link to="/">About</Link>
+          </div>          
+          <div className="right">
+            <Link to="/">Contact</Link>
+          </div>
+          <div className="right">
+            <Link to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>      
+
+      </div>
+    </div>
   )
 }
 
