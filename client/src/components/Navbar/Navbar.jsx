640adffe98ea09feafafb8a69154084386410086
quantummm <aidenwang0826@gmail.com>
2023-03-17 23:13:43 +1100

    fix: add classname to div

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -21,30 +21,30 @@ const Navbar = () => {
             <KeyboardArrowDownIcon />
           </div>
           <div className="item">
-            <Link to="/products/1">Women</Link>
+            <Link className="link" to="/products/1">Women</Link>
           </div>
           <div className="item">
-            <Link to="/products/2">Men</Link>
+            <Link className="link" to="/products/2">Men</Link>
           </div>
           <div className="item">
-            <Link to="/products/3">Children</Link>
+            <Link className="link" to="/products/3">Children</Link>
           </div>
         </div>
         <div className='center'>
-          <Link to="/">LAMASTORE</Link>
+          <Link className="link" to="/">LAMASTORE</Link>
         </div>
         <div className='right'>
-          <div className="right">
-            <Link to="/">Homepage</Link>
+          <div className="item">
+            <Link className="link" to="/">Homepage</Link>
           </div>          
-          <div className="right">
-            <Link to="/">About</Link>
+          <div className="item">
+            <Link className="link" to="/">About</Link>
           </div>          
-          <div className="right">
-            <Link to="/">Contact</Link>
+          <div className="item">
+            <Link className="link" to="/">Contact</Link>
           </div>
-          <div className="right">
-            <Link to="/">Stores</Link>
+          <div className="item">
+            <Link className="link" to="/">Stores</Link>
           </div>
           <div className="icons">
             <SearchIcon />
