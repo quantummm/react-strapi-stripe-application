c66f90feac3c8a9a5b984ed55d6baf90b120c70f
quantummm <aidenwang0826@gmail.com>
2023-03-29 23:15:11 +1100

    feat: add image slider components

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "./Home.scss"
+import Slider from '../../components/Slider/Slider'
+import  FeaturedProducts  from '../../components/FeaturedProducts/FeaturedProducts'
+import Categories from '../../components/Categories/Categories'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Slider />
+      <FeaturedProducts type="featured" />
+      <Categories />
+      <FeaturedProducts type="trending" />
+    </div>
   )
 }
 
